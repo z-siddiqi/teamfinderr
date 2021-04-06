e69b7546d1e307f1e1d8071d751af61fbb929de7
George Goodall <george.goodall@hotmail.co.uk>
2021-04-06 10:11:13 +0100

    add profile model

--- config/urls.py
+++ config/urls.py
@@ -18,8 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
     path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')),
     path('api/v1/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('api/v1/projects/', include('projects.urls')),
+   
 ]
