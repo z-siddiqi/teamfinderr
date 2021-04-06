ea8fd8dd7288f8a92c0a5a20bb92fcd3439664d6
George Goodall <george.goodall@hotmail.co.uk>
2021-04-06 10:20:10 +0100

    add admin UserProfile

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import Skill, UserProfile
 
 # Register your models here.
+admin.site.register(Skill)
 admin.site.register(UserProfile)
