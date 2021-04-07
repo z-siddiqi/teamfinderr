ceeee4bfd4f1d1769deeb35ee9b7aa3951c3f61e
Jaron <jaronescoo@gmail.com>
2021-04-07 14:11:12 +0100

    adding 'status' for requests  options in project/models; adding update to request

--- projects/views.py
+++ projects/views.py
@@ -3,16 +3,12 @@ from rest_framework import viewsets
 
 from .models import Project, ProjectMembership, ProjectMembershipRequest
 from .serializers import ProjectSerializer, ProjectMembershipSerializer, ProjectMembershipRequestSerializer
-from rest_framework.response import Response
+from rest_framework.response import Response, BasePermission
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
-
-    # def perform_create(self, serializer):
-    #     serializer.save(owner=self.request.user)
-
 
 class ProjectMembershipViewSet(viewsets.ModelViewSet):
     serializer_class = ProjectMembershipSerializer
@@ -21,6 +17,13 @@ class ProjectMembershipViewSet(viewsets.ModelViewSet):
         project = Project.objects.get(pk=self.kwargs["project_pk"])
         members = ProjectMembership.objects.filter(project=project)
         return members
+
+class IsMember(BasePermission):
+    
+    def has_object_permission(self, request, view, obj):
+        if request.method == "PATCH":
+            project = obj.to_project
+            return project.project_memberships.filter(user=request.user.profile).exists()
 
 class ProjectMembershipRequestViewSet(viewsets.ModelViewSet):
     serializer_class = ProjectMembershipRequestSerializer
@@ -34,25 +37,8 @@ class ProjectMembershipRequestViewSet(viewsets.ModelViewSet):
         project = Project.objects.get(pk=self.kwargs['project_pk'])
         serializer.save(from_user=self.request.user.profile,to_project=project)
 
-    def update(self, serializer, **kwargs):
-        #permission required so that only project members can update the project request status
-        #when status set to accepted, user id added to Project members list
-        #query project member list, if token associated w/ one of users then access
-
-        project = Project.objects.get(pk=self.kwargs["project_pk"])
-        members = ProjectMembership.objects.filter(project=project)
-        if members.filter(user=self.request.user.profile).exists():
-            status = request.data['status']
-            request = self.get_object()
-            request.status = status 
-            request.save()
-            serializer = ProjectMembershipRequestSerializer(data=request)
-            print(status)
-            #return Response(serializer.data)
-        #kwargs['partial'] = True
-        #return super().update(request, *args, **kwargs)
-            
-
-            #membership_request.to_project.members.add(membership_request=from_user)
-        #need to add new project to users projects portfolio
+    def update(self, serializer, *args, **kwargs):
+        kwargs['partial'] = True
+        return super().update(request, *args, **kwargs)
+        
         
