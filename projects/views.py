6d6fd6aeb414dd656e189ce33192628dfce77c55
Jaron <jaronescoo@gmail.com>
2021-04-07 14:14:53 +0100

    adding 'status' for requests  options in project/models; adding update to request

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from rest_framework.permissions import IsAuthenticated, BasePermission
 
 from .models import Project, ProjectMembership, ProjectMembershipRequest
