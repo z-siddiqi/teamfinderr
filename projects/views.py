b3cbd64687a03ed6e500ed217d84f5ba86b4c0ae
Jaron <jaronescoo@gmail.com>
2021-04-07 12:39:50 +0100

    NW UpdateR Views, introduced 'status' options  in project/models

--- projects/views.py
+++ projects/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 
 from .models import Project, ProjectMembership, ProjectMembershipRequest
 from .serializers import ProjectSerializer, ProjectMembershipSerializer, ProjectMembershipRequestSerializer
+from rest_framework.response import Response
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
@@ -32,3 +33,26 @@ class ProjectMembershipRequestViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         project = Project.objects.get(pk=self.kwargs['project_pk'])
         serializer.save(from_user=self.request.user.profile,to_project=project)
+
+    def update(self, serializer, **kwargs):
+        #permission required so that only project members can update the project request status
+        #when status set to accepted, user id added to Project members list
+        #query project member list, if token associated w/ one of users then access
+
+        project = Project.objects.get(pk=self.kwargs["project_pk"])
+        members = ProjectMembership.objects.filter(project=project)
+        if members.filter(user=self.request.user.profile).exists():
+            status = request.data['status']
+            request = self.get_object()
+            request.status = status 
+            request.save()
+            serializer = ProjectMembershipRequestSerializer
+            print(status)
+            #return Response(serializer.data)
+        #kwargs['partial'] = True
+        #return super().update(request, *args, **kwargs)
+            
+
+            #membership_request.to_project.members.add(membership_request=from_user)
+        #need to add new project to users projects portfolio
+        
