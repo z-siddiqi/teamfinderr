6a87984e75969dbaf8cab4f5473d84c323018fb2
Jaron <jaronescoo@gmail.com>
2021-04-08 17:01:17 +0100

    add role serializer, role name when user added to project, updated other serializers with roles

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,11 +1,18 @@
 from rest_framework import serializers
 
-from .models import Project, ProjectMembership, ProjectMembershipRequest
+from .models import Project, ProjectMembership, ProjectMembershipRequest, Role
 
 from profiles.serializers import UserProfileSerializer
 
+class RoleSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        fields = ('name', 'id')
+        model = Project
+
 
 class ProjectSerializer(serializers.ModelSerializer):
+    roles = RoleSerializer(many=True, read_only=True)
 
     class Meta:
         fields = ('id', 'name', 'description','members','roles') 
@@ -14,16 +21,17 @@ class ProjectSerializer(serializers.ModelSerializer):
 
 class ProjectMembershipSerializer(serializers.ModelSerializer):
     user = UserProfileSerializer(read_only=True)
+    role = RoleSerializer(read_only=True)
 
     class Meta:
         fields = ('user', 'message','role')
         model = ProjectMembership
 # we need to fix the serialiser to not return all of the users properties#
 
-
 class ProjectMembershipRequestSerializer(serializers.ModelSerializer):
     to_project = ProjectSerializer(read_only=True)
     from_user = UserProfileSerializer(read_only=True)
+    role = RoleSerializer(read_only=True)
 
     class Meta:
         fields = ('id', 'to_project', 'from_user', 'status','role')
@@ -35,4 +43,4 @@ class ProjectMembershipRequestNoStatusSerializer(ProjectMembershipRequestSeriali
         fields = ('id','to_project','from_user','status','role')
         read_only_fields = ('status',)
         model = ProjectMembershipRequest
-        
+    
