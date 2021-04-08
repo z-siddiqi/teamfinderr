e9585fa99f8584639f0f7ab18416bba05d9f45c1
Shashi <shashi.k.parekh@bath.edu>
2021-04-08 08:27:55 +0100

    add route for viewing user profile skills and remove 'skills' field from UserProfileSerializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -14,8 +14,15 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
     user_id = serializers.IntegerField(source="user.id", read_only=True)  # UserProfile does not have direct access to this field
     username = serializers.CharField(source="user.username", read_only=True)  # UserProfile does not have direct access to this field
-    skills = serializers.StringRelatedField(many=True, read_only=True)
+    # skills = serializers.StringRelatedField(many=True, read_only=True)
 
     class Meta:
         model = UserProfile
-        fields = ["user_id", "username", "bio", "skills"]
+        fields = ["user_id", "username", "bio"]
+        
+class UserProfileSkillSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Skill
+        fields = ["name", "type"]
+
--- profiles/urls.py
+++ profiles/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
-from rest_framework.routers import SimpleRouter
+from rest_framework_nested import routers
 
-from .views import UserProfileViewSet, UserProfileSearchView,Skill
+from .views import UserProfileViewSet, UserProfileSearchView, UserProfileSkillsViewSet
 
-router = SimpleRouter()
-router.register('', UserProfileViewSet, basename='profile')
+router = routers.SimpleRouter()
+router.register('', UserProfileViewSet, basename='profiles')
+
+user_skills_router = routers.NestedSimpleRouter(router,'',lookup='profile')
+user_skills_router.register('skills',UserProfileSkillsViewSet,basename='profile-skills')
 
 urlpatterns = [
     path("search/", UserProfileSearchView.as_view(), name="profile-search"),
 ]
 
-urlpatterns += router.urls
+urlpatterns += router.urls + user_skills_router.urls
