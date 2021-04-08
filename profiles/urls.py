d3f32d43acc0a5af1aa5222e8159497da1e91202
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2021-04-08 14:31:16 +0100

    add user_projects nested router

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
-from rest_framework.routers import SimpleRouter
+from rest_framework_nested import routers
 
-from .views import UserProfileViewSet, UserProfileSearchView
+from .views import UserProfileViewSet, UserProfileSearchView, UserProfileProjectViewSet
 
-router = SimpleRouter()
+router = routers.SimpleRouter()
 router.register('', UserProfileViewSet, basename='profile')
+
+user_projects_router = routers.NestedSimpleRouter(router, '', lookup='profile')
+user_projects_router.register('projects', UserProfileProjectViewSet, basename='profile-projects')
 
 urlpatterns = [
     path("search/", UserProfileSearchView.as_view(), name="profile-search"),
 ]
 
-urlpatterns += router.urls
+urlpatterns += router.urls + user_projects_router.urls
