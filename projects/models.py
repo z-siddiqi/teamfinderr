ad0ffec546a719628147c924ab639d133de51bc7
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2021-04-08 11:04:42 +0100

    add CompletedProjectsManager to Projects model

--- projects/models.py
+++ projects/models.py
@@ -31,6 +31,12 @@ class Role(models.Model): # Role is the job role that a project requires  e.g. p
 # class Difficulty(models.Model):
 #     pass # project difficulty (Used to determine candidates suitability)
 
+
+class CompletedProjectsManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(date__gt=self.end_date)
+
+
 class Project(models.Model): # a project that a user will create
     name = models.CharField(max_length=200) #charfield limited to 255 characters
     description = models.TextField(max_length=500) #Textfield >255 characters
@@ -42,6 +48,8 @@ class Project(models.Model): # a project that a user will create
     #difficulty = models.PositiveIntegerField(validators=[MaxValueValidator(5),MinValueValidator(1)])
     start_date = models.DateTimeField(auto_now_add=True)
     end_date = models.DateTimeField(null=True,blank=True)
+    objects = models.Manager()
+    completed_objects = CompletedProjectsManager()
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
@@ -88,6 +96,3 @@ class ProjectMembershipRequest(models.Model):
 
     def __str__(self):
         return f'{self.from_user} to {self.to_project}'
-
- 
-    
