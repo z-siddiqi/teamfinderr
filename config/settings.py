1b30cca5e82f7d0f5a20312982db08822b5f9059
Issy Hanna <ih315@cam.ac.uk>
2021-04-08 09:25:03 +0100

    add session authentication for documentation

--- config/settings.py
+++ config/settings.py
@@ -143,6 +143,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer', #Changed from browsable API to JSON Renderer
--- config/urls.py
+++ config/urls.py
@@ -33,6 +33,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')),
     path('api/v1/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('api/v1/projects/', include('projects.urls')),
