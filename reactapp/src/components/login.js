96777729f69e9515696a723a47969afcf62f7546
Adam Nash <adamnash3112@gmail.com>
2021-04-21 10:54:45 +0100

    Handle login successfully with CSRF token and universal cookie.

--- reactapp/src/components/login.js
+++ reactapp/src/components/login.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
 
 const API_HOST = 'http://localhost:8000';
 
@@ -21,7 +23,8 @@ export default class Login extends Component {
     state = {
         username: '',
         email: '',
-        password: ''
+        password: '',
+        error: ''
       }
 
     redirectHome = event => {
@@ -47,35 +50,53 @@ export default class Login extends Component {
         }
 
     async componentDidMount() {
-        _csrfToken = await getCsrfToken();
+        //_csrfToken = await getCsrfToken();
         }
 
     handleSubmit = (event) => {
         event.preventDefault();
        
-        console.log(_csrfToken);
+        //console.log(_csrfToken);
 
         const user = {
             username: this.state.username,
             email: this.state.email,
-            password: this.state.password
+            password: this.state.password,
 
         }
 
         const options = {
             headers: {
                 'Content-Type': 'application/json',
-                'Cookie' : `csrftoken=${_csrfToken}`
+                //'Cookie' : `csrftoken=${_csrfToken}`,
+                "X-CSRFToken": cookies.get("csrftoken"),
             }
           };
 
         console.log(user)
 
-        axios.post(`http://127.0.0.1:8000/api/v1/dj-rest-auth/login/`, { user }, options)
+        axios.post(`http://127.0.0.1:8000/api/v1/dj-rest-auth/login/`, { username: this.state.username, password: this.state.password }, options)
         .then(res => {
           console.log(res);
           console.log(res.data);
-        })
+
+          if(res.status==200){
+            console.log("Log in success");
+            this.props.history.push('/home');
+          } else{
+            console.log("Log in failed, Wrong username or password.")
+          }
+        }).catch((err) => {
+            console.log(err);
+            this.setState({error: "Wrong username or password."})
+        });
+        /*.then(() => {
+            console.log(this.state)
+            if (this.state.error===""){
+                this.props.history.push('/home');
+            }
+        });
+        */
       }
 
     render() {
@@ -106,7 +127,7 @@ export default class Login extends Component {
                     </div>
                 </div>
 
-                <button type="submit" onSubmit={this.redirectHome} className="btn btn-dark btn-lg btn-block">Sign in</button>
+                <button type="submit" className="btn btn-dark btn-lg btn-block">Sign in</button>
                 <p className="forgot-password text-right">
                     <button onClick={this.redirectForgotPassword}>Forgot password?</button>
                 </p>
--- reactapp/src/components/projectlist.js
+++ reactapp/src/components/projectlist.js
@@ -1,18 +1,39 @@
 import React, { Component } from "react";
+import axios from "axios";
 //import { BrowserRouter as Link } from 'react-router-dom'
 //import { useHistory } from 'react-router-dom';
 
 export default class ProjectListView extends Component {
 
+    state = {
+        projects: [],
+        project: {},
+        isProjectViewOn: false,
+        sortValue: '',
+        inputValue: '',
+
+    }
+
     projectList(){
         this.props.history.push('/project-list')
     }
+
+    fetchProjects = async () => {
+    const { data } = await axios.get(
+        "http://127.0.0.1:8000/api/v1/projects/"
+    );
+    console.log(data);
+    return { data };
+    };
+
+
     
     render() {
         return (
         <>
-            <div>
+            <div  onLoad = {this.fetchProjects}>
                 <h1>Projects</h1>
+               
                 <br/>
                 <h4>Search</h4>
                 <div class="input-group">
