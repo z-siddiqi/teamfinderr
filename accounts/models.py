f2d89e931600ecc6962df7971050517b96009605
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2021-05-20 23:23:16 +0100

    add is_guest to CustomUser model

--- accounts/models.py
+++ accounts/models.py
@@ -2,8 +2,5 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class CustomUser(
-    AbstractUser
-):  # inherits typical properties from AbstractUser ->  E.g. username, email etc
-    def __str__(self):
-        return f"{self.username}"
+class CustomUser(AbstractUser):
+    is_guest = models.BooleanField(default=False)
