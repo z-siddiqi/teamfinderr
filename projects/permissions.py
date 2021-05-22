6d73bad7a50c9c4a4adec57cc1587bec66ece443
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2021-05-22 17:05:41 +0100

    refactor IsMember permission

--- projects/permissions.py
+++ projects/permissions.py
@@ -1,10 +1,8 @@
-from rest_framework.permissions import IsAuthenticated, BasePermission
+from rest_framework.permissions import BasePermission
 
 
 class IsMember(BasePermission):
+    # not applied when creating objects
     def has_object_permission(self, request, view, obj):
-        if request.method == "PATCH":
-            project = obj.to_project
-            return project.project_memberships.filter(
-                user=request.user.profile
-            ).exists()
+        project = obj.to_project
+        return project.memberships.filter(user=request.user).exists()
