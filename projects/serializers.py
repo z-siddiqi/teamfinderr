19163f74f99464ac039340c80cbd43dd5cad8fb5
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2021-05-22 22:03:18 +0100

    refactor ProjectSerializer

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,16 +1,11 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
 
 from .models import Project, ProjectMembership, ProjectMembershipRequest
 from accounts.serializers import CustomUserSerializer
 
 User = get_user_model()
-
-
-class ProjectSerializer(serializers.ModelSerializer):
-    class Meta:
-        fields = ("id", "name", "description", "members", "roles")
-        model = Project
 
 
 class ProjectMembershipSerializer(serializers.ModelSerializer):
@@ -19,6 +14,18 @@ class ProjectMembershipSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProjectMembership
         fields = ["user", "role"]
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    members = serializers.SerializerMethodField()
+
+    def get_members(self, obj):
+        qset = ProjectMembership.objects.filter(project=obj)
+        return [ProjectMembershipSerializer(m).data for m in qset]
+
+    class Meta:
+        model = Project
+        fields = ["id", "name", "description", "members"]
 
 
 class ProjectMembershipRequestSerializer(serializers.ModelSerializer):
