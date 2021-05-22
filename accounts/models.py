cf933876a4ae525ea22fcf63483b03f54601cfd2
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2021-05-22 15:23:58 +0100

    add bio and skills to CustomUser model

--- accounts/models.py
+++ accounts/models.py
@@ -2,5 +2,25 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class SkillCategoryMixin(models.Model):
+    CATEGORY_CHOICES = (
+        ("relationship", "Relationship"),
+        ("communication", "Communication"),
+        ("management", "Management"),
+        ("analytical", "Analytical"),
+        ("creative", "Creative"),
+        ("technical", "Technical"),
+    )
+    category = models.CharField(max_length=13, choices=CATEGORY_CHOICES)
+
+
+class Skill(SkillCategoryMixin):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.name} ({self.category})"
+
+
 class CustomUser(AbstractUser):
-    is_guest = models.BooleanField(default=False)
+    bio = models.CharField(max_length=255, blank=True)
+    skills = models.ManyToManyField(Skill, blank=True, related_name="users")
