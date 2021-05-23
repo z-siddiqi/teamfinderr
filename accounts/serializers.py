aa95d6fd9f7b6a2e8e2c1b39769bf3123785015c
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2021-05-23 20:49:34 +0100

    add memberships field to CustomUserDetailSerializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,23 +1,39 @@
 from rest_framework import serializers
 
 from .models import CustomUser, Skill
+from projects.models import Project, ProjectMembership
 
 
 class SkillSerializer(serializers.ModelSerializer):
     class Meta:
         model = Skill
-        fields = ("name", "category")
+        fields = ["id", "name", "category"]
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ["id", "name", "description"]
+
+
+class ProjectMembershipSerializer(serializers.ModelSerializer):
+    project = ProjectSerializer(read_only=True)
+    
+    class Meta:
+        model = ProjectMembership
+        fields = ["id", "role", "category", "project"]
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ("id", "username")
+        fields = ["id", "username"]
 
 
 class CustomUserDetailSerializer(serializers.ModelSerializer):
     skills = SkillSerializer(many=True, read_only=True)
+    memberships = ProjectMembershipSerializer(many=True, read_only=True)
 
     class Meta:
         model = CustomUser
-        fields = ("id", "username", "first_name", "last_name", "bio", "skills")
+        fields = ["id", "username", "first_name", "last_name", "bio", "skills", "memberships"]
