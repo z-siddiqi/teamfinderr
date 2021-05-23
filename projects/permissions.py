a2763dd27f226c1e57351a6b86c3fc3d952eb585
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2021-05-23 17:09:54 +0100

    update IsMember permission to reflect ProjectMembershipRequest model changes

--- projects/permissions.py
+++ projects/permissions.py
@@ -4,5 +4,5 @@ from rest_framework.permissions import BasePermission
 class IsMember(BasePermission):
     # not applied when creating objects
     def has_object_permission(self, request, view, obj):
-        project = obj.to_project
+        project = obj.to_membership.project
         return project.memberships.filter(user=request.user).exists()
