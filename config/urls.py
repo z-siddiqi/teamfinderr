23e5bfff9bb9d1a0969496e62de1ce8d7395bb14
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2021-05-23 19:32:34 +0100

    set up project level urls

--- config/urls.py
+++ config/urls.py
@@ -1,33 +1,34 @@
-"""config URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_nested.routers import NestedDefaultRouter
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
+from accounts.views import CustomUserAddSkillView, CustomUserRemoveSkillView, CustomUserSearchView
+from projects.views import ProjectViewSet, ProjectMembershipViewSet, ProjectMembershipRequestViewSet
 
 schema_view = get_schema_view(
     openapi.Info(title="teamfinderr", default_version="v1"),
     public=True,
 )
 
+router = DefaultRouter()
+router.register(r"requests", ProjectMembershipRequestViewSet)
+router.register(r"projects", ProjectViewSet)
+
+projects_router = NestedDefaultRouter(router, r"projects", lookup="project")
+projects_router.register(r"memberships", ProjectMembershipViewSet, basename="project-memberships")
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("api/v1/auth/", include("dj_rest_auth.urls")),
+    path("api/v1/auth/user/add-skill/", CustomUserAddSkillView.as_view(), name="user-add-skill"),
+    path("api/v1/auth/user/remove-skill/", CustomUserRemoveSkillView.as_view(), name="user-remove-skill"),
     path("api/v1/auth/registration/", include("dj_rest_auth.registration.urls")),
-    path("api/v1/projects/", include("projects.urls")),
+    path("api/v1/", include(router.urls)),
+    path("api/v1/", include(projects_router.urls)),
+    path("api/v1/search/", CustomUserSearchView.as_view(), name="user-search"),
     path("docs/", schema_view.with_ui("swagger", cache_timeout=0), name="schema"),
 ]
