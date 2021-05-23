ab32521d9a843fe21819912ece5285633e497c37
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2021-05-23 19:19:00 +0100

    add ProjectMembershipViewSet

--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,9 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
 from .models import Project, ProjectMembership, ProjectMembershipRequest
-from .serializers import ProjectSerializer, ProjectMembershipRequestSerializer
+from .serializers import ProjectSerializer, ProjectMembershipDetailSerializer, ProjectMembershipRequestSerializer
 from .permissions import IsMember
 
 
@@ -10,11 +11,29 @@ from .permissions import IsMember
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+    lookup_field = "id"  # make the nested router parent lookup regex project_id
 
     def perform_create(self, serializer):
         user = self.request.user
         project = serializer.save()
         ProjectMembership.objects.create(role="owner", category="management", user=user, project=project)
+
+
+class ProjectMembershipViewSet(viewsets.ModelViewSet):
+    serializer_class = ProjectMembershipDetailSerializer
+
+    def get_project(self):
+        project = get_object_or_404(Project, pk=self.kwargs["project_id"])
+        return project
+
+    def get_queryset(self):
+        project = self.get_project()
+        memberships = ProjectMembership.objects.filter(project=project)
+        return memberships
+
+    def perform_create(self, serializer):
+        project = self.get_project()
+        serializer.save(project=project)
 
 
 class ProjectMembershipRequestViewSet(viewsets.ModelViewSet):
