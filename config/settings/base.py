3eb9a4c97e9d05f31ce556b2e3f3ff8eb3bd34fc
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2022-09-11 11:20:17 +0100

    update settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -17,7 +17,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", default=True)
 
-ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
